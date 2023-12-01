64683c42991052ce2a076bd373f68670608751b5
Justin <113915120+justinromanowski@users.noreply.github.com>
2023-11-30 21:17:21 -0500

    Add files via upload
    Up to date versions of main & GPIO functions as of 11/30/2023

--- blackbeard_main.py
+++ blackbeard_main.py
@@ -1,11 +1,16 @@
 import RPi.GPIO as GPIO
 import time
-import gpio_functions as GPIO_fun
+import gpio_functions as GPIO_func
 
-LOCK_PIN = 00 # pin 37 bcm 26
-CHUTE_PIN = 26 # pin 37 bcm 26
+# Initialize GPIO pins
+LOCK_PIN = 5 # pin x bcm 5
+CHUTE_PIN = 22 # pin 37 bcm 22
 TRIG_PIN = 18 # pin 12 bcm 18
 ECHO_PIN = 12 # pin 32 bcm 12
+GLED_PIN = 13 # pin x bcm 13
+BLED_PIN = 26 # pin x bcm 26
+CBUTTON_PIN = 20 # pin x bcm 20
+LBUTTON_PIN = 21 # pin x bcm 21
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LOCK_PIN, GPIO.OUT)
@@ -13,21 +18,52 @@ GPIO.setup(CHUTE_PIN, GPIO.OUT)
 GPIO.setup(TRIG_PIN, GPIO.OUT)
 GPIO.output(TRIG_PIN, GPIO.LOW)
 GPIO.setup(ECHO_PIN, GPIO.IN)
+GPIO.setup(GLED_PIN, GPIO.OUT)
+GPIO.setup(BLED_PIN, GPIO.OUT)
+GPIO.setup(CBUTTON_PIN, GPIO.IN)
+GPIO.setup(LBUTTON_PIN, GPIO.IN)
+
 time.sleep(1)
 
+# Setup both servo motors
 chute = GPIO.PWM(CHUTE_PIN, 50) # GPIO 17 for PWM with 50Hz
 chute.start(2.5) # Initialization to 0
 
+lock = GPIO.PWM(LOCK_PIN, 50)
+lock.start(2.5)
+
+GPIO.output(GLED_PIN, GPIO.LOW)
+GPIO.output(BLED_PIN, GPIO.LOW)
+
+def chute_activation():
+	time.sleep(0.2)
+	GPIO_func.open_chute(chute)
+	time.sleep(5)
+	print("sensing movement...")
+	GPIO_func.sense_movement()
+	GPIO_func.close_chute(chute)
+	print("finished")
+
+def lock_activation():
+	time.sleep(0.2)
+	print("locking door")
+	GPIO_func.unlock_door(lock)
+	time.sleep(3)
+	print("locking door")
+	GPIO_func.lock_door(lock)
+	print("finished")
+
 try:
 	while True:
-		GPIO_fun.servo_write(0)
-		time.sleep(0.5)
-		GPIO_fun.servo_write(60)
-		time.sleep(5)
-		print("sensing movement...")
-		GPIO_fun.sense_movement()
-		GPIO_fun.servo_write(0)
-		print("finished")
+		# check if buttons are pressed, then do some action (open chute or unluck)
+		chute_active = GPIO.input(CBUTTON_PIN)
+		lock_active = GPIO.input(LBUTTON_PIN)
+		print(f"CHUTE = {chute_active} | LOCK = {lock_active}")
+
+		if not chute_active:
+			chute_activation()
+		elif not lock_active:
+			lock_activation()
 
 except KeyboardInterrupt:
 	chute.stop()
--- gpio_functions.py
+++ gpio_functions.py
@@ -1,25 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 
-def get_distance(trigger_pin,echo_pin):
+
+# Initialize GPIO pins
+LOCK_PIN = 5 # pin x bcm 5
+CHUTE_PIN = 22 # pin 37 bcm 22
+TRIG_PIN = 18 # pin 12 bcm 18
+ECHO_PIN = 12 # pin 32 bcm 12
+GLED_PIN = 13 # pin x bcm 13
+BLED_PIN = 26 # pin x bcm 26
+CBUTTON_PIN = 20 # pin x bcm 20
+LBUTTON_PIN = 21 # pin x bcm 21
+
+
+def get_distance():
 	"""
 	Sends an ultrasonic trigger, listens for the echo, & calculates the distance.
 	Returns distance.
 	"""
 
 	# send pulse to trigger pin to send ultrasound wave
-	GPIO.output(trigger_pin,GPIO.HIGH)
+	GPIO.output(TRIG_PIN,GPIO.HIGH)
 	time.sleep(0.00001)
-	GPIO.output(trigger_pin,GPIO.LOW)
+	GPIO.output(TRIG_PIN,GPIO.LOW)
 	print("sent trigger")
 	# start recording time & wait for ultrasound to return
 	start_time = time.time()
 	stop_time = time.time()
 
 	print("waiting for echo")
-	while GPIO.input(echo_pin) == 0:
+	while GPIO.input(ECHO_PIN) == 0:
 		start_time = time.time()
-	while GPIO.input(echo_pin) == 1:
+	while GPIO.input(ECHO_PIN) == 1:
 		stop_time = time.time()
 
 	print("calculating distance")
@@ -85,38 +97,65 @@ def sense_movement():
 			movement_sensed = True
 
 			print(f"movement detected in chute1 reseting timers..")
-			
+
 def servo_write(angle,pwm_obj):
 	"""
 	Changes the angle of the servo motor. Desired angle passed in as argument
 	"""
-	
+
 	# pwm ranges (0-10) + 2.5 --> 2.5-12.5
 	# angle ranges 0-180
 	pwm = (angle/18) + 2.5
 	pwm_obj.ChangeDutyCycle(pwm)
 	print(f"angle: {angle}\tpwm: {pwm}")
 
+def led_blink(led_pin):
+	"""
+	Blinks the LED pin passed into function
+	"""
+	for i in range(3):
+		GPIO.output(led_pin, GPIO.LOW)
+		time.sleep(0.5)
+		GPIO.output(led_pin, GPIO.HIGH)
+		time.sleep(0.5)
+
 def open_chute(pwm_obj):
 	""" 
  	Opens the package chute door 
   	"""
-	servo_write(60,pwm_obj)
+	led_blink(BLED_PIN)
+	GPIO.output(BLED_PIN, 1)
+
+	for i in range(0,62,2):
+                servo_write(i,pwm_obj)
+                time.sleep(0.1)
 
 def close_chute(pwm_obj):
 	""" 
  	Closes the package chute door
   	"""
-	servo_write(0,pwm_obj)
+	led_blink(BLED_PIN)
+	GPIO.output(BLED_PIN, 0)
+
+	for i in range(60,-2,-2):
+                servo_write(i,pwm_obj)
+                time.sleep(0.1)
 
 def unlock_door(pwm_obj):
 	""" 
  	Unlocks the deadbolt on the door
   	"""
-	servo_write(60,pwm_obj)
+	servo_write(135,pwm_obj)
+
+	led_blink(GLED_PIN)
+	GPIO.output(GLED_PIN, 1)
 
 def lock_door(pwm_obj):
 	""" 
  	Locks the deadbolt on the door
   	"""
+	led_blink(GLED_PIN)
+	GPIO.output(GLED_PIN, 0)
+
 	servo_write(0,pwm_obj)
+
